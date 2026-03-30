08d5c8a2d38d50ce76420aed4d060c5451869306
Penny Chase <mpchase@mac.com>
2026-03-29 23:37:05 -0400

    Created queue and tests with Copilot

--- lib/queue.ex
+++ lib/queue.ex
@@ -2,17 +2,59 @@ defmodule Queue do
   @moduledoc """
   Documentation for `Queue`.
   """
+  defstruct [:items]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Queue.hello()
-      :world
-
+  Creates a new empty queue.
   """
-  def hello do
-    :world
+  def new() do
+    %Queue{items: []}
   end
+
+  @doc """
+  Enqueues an item into the queue.
+  """
+  def enqueue(queue, item) do
+    %Queue{items: queue.items ++ [item]}
+  end
+
+
+  @spec dequeue(any()) :: {any(), any()}
+  @doc """
+  Dequeues an item from the queue. Returns a tuple of the dequeued item and the updated queue.
+  """
+  def dequeue(queue) do
+    case queue.items do
+      [] ->
+        {nil, queue}
+      [head | tail] ->
+        {head, %Queue{items: tail}}
+    end
+  end
+
+  @doc """
+  Checks if the queue is empty.
+  """
+  def empty?(queue) do
+    Enum.empty?(queue.items)
+  end
+
+  @doc """
+  Returns the number of items in the queue.
+  """
+  def size(queue) do
+    length(queue.items)
+  end
+
+  @doc """
+  Returns the item at the front of the queue without removing it.
+  """
+  def peek(queue) do
+    case queue.items do
+      [] -> nil
+      [head | _] -> head
+    end
+  end
+
+
 end
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -2,7 +2,40 @@ defmodule QueueTest do
   use ExUnit.Case
   doctest Queue
 
-  test "greets the world" do
-    assert Queue.hello() == :world
+  test "creates a new empty queue" do
+    queue = Queue.new()
+    assert Queue.empty?(queue) == true
+    assert Queue.size(queue) == 0
+  end
+
+  test "enqueues and dequeues items" do
+    queue = Queue.new()
+    queue = Queue.enqueue(queue, 1)
+    queue = Queue.enqueue(queue, 2)
+    queue = Queue.enqueue(queue, 3)
+
+    assert Queue.size(queue) == 3
+    assert Queue.peek(queue) == 1
+
+    {item, queue} = Queue.dequeue(queue)
+    assert item == 1
+    assert Queue.size(queue) == 2
+    assert Queue.peek(queue) == 2
+
+    {item, queue} = Queue.dequeue(queue)
+    assert item == 2
+    assert Queue.size(queue) == 1
+    assert Queue.peek(queue) == 3
+
+    {item, queue} = Queue.dequeue(queue)
+    assert item == 3
+    assert Queue.empty?(queue) == true
+  end
+
+  test "dequeue from an empty queue" do
+    queue = Queue.new()
+    {item, queue} = Queue.dequeue(queue)
+    assert item == nil
+    assert Queue.empty?(queue) == true
   end
 end
